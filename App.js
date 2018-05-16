8f6ab3db73e2b544e6eaa6adc3604309ff6cd005
Marcus Bernales <marcusbernales@gmail.com>
2018-05-16 15:37:13 -0700

    Data - Making network request to get dad joke

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
+const DAD_JOKE_API = 'https://icanhazdadjoke.com'
+const headers = {
+  Accept: 'application/json',
+}
+
 export default class App extends React.Component {
+  state = {
+    joke: 'This is not a joke',
+    loading: false
+  }
+  fetchJoke = () => {
+    fetch(DAD_JOKE_API, { headers })
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+        this.setState({ joke: json.joke })
+      })
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Joke goes here</Text>
-        <Button>joke pls</Button>
+        <Text>{this.state.joke}</Text>
+        <Button title="joke pls" onPress={this.fetchJoke} />
       </View>
     );
   }
