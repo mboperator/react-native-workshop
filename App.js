e5f3716a7eed2d6bd08a1491374c607442a311db
Marcus Bernales <marcusbernales@gmail.com>
2018-05-16 15:43:44 -0700

    Extra Credit - Added a photo of a dad and some styles

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react'
+import { TouchableOpacity, Image, StyleSheet, Text, View } from 'react-native'
 
 const DAD_JOKE_API = 'https://icanhazdadjoke.com'
+const DAD_IMAGE =
+  'https://pbs.twimg.com/profile_images/3078903617/a66c58171a99f27f9a876f2fc9877a0c_400x400.jpeg'
 const headers = {
-  Accept: 'application/json',
+  Accept: 'application/json'
 }
 
 export default class App extends React.Component {
@@ -22,10 +24,17 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>{this.state.joke}</Text>
-        <Button title="joke pls" onPress={this.fetchJoke} />
+        <View style={{ paddingBottom: 50 }}>
+          <Text style={{ textAlign: 'center' }}>{this.state.joke}</Text>
+        </View>
+        <TouchableOpacity onPress={this.fetchJoke}>
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{ uri: DAD_IMAGE }}
+          />
+        </TouchableOpacity>
       </View>
-    );
+    )
   }
 }
 
@@ -34,6 +43,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
